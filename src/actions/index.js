370b1c41c565cc91749b752e8762ed7196154056
Chuck Conway <charles@cconway.com>
2016-05-13 21:22:20 -0700

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import {browserHistory} from 'react-router';
+import {AUTH_USER, UNAUTH_USER, AUTH_ERROR} from './types';
+
+
+const ROOT_URL = 'http://localhost:3090';
+
+export function signinUser({email, password}){
+  return function(dispatch){
+      //submit email/password to the server
+      axios.post(`${ROOT_URL}/signin`, {email, password})
+      .then(res => {
+        // if success
+
+        //update the state to indicate the user is authenticated
+        dispatch({type:AUTH_USER});
+
+        localStorage.setItem('token', res.data.token);
+
+
+        browserHistory.push('/feature');
+      })
+      .catch(()=>{
+          //dispatch({type:UNAUTH_USER});
+          dispatch(authError('bad login info'));
+      });
+
+
+      //if request is good.
+
+
+
+      //if request is bad.
+  }
+}
+
+export function authError(error){
+  return {
+    type:AUTH_ERROR,
+    payload:error
+  }
+}
+
+export function signoutUser() {
+  localStorage.removeItem('token');
+
+  return {type:UNAUTH_USER}
+}
